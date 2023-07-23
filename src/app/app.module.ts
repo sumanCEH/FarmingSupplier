a0472f138e4eff72c1925f4882f4c1dcbe010c82
PATEL <uditya.patel@capgemini.com>
2023-07-23 21:48:41 +0530

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,78 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+// import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AuthService } from './auth.service';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AuthGuard } from './auth.guard';
+import { SupplierDashboardComponent } from './supplier-dashboard/supplier-dashboard.component';
+import { FarmerDashboardComponent } from './farmer-dashboard/farmer-dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ViewComplaintComponent } from './view-complaint/view-complaint.component';
+import { UpdateComplaintComponent } from './update-complaint/update-complaint.component';
+// import { AuthGuard } from './auth.guard';
+
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signup', component: SignupComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'about-us', component: AboutUsComponent},
+  { path: 'admin', component: AdminDashboardComponent, canActivate: [AuthGuard], data : {role: 'ROLE_ADMIN'} },
+  { path: 'farmer', component: FarmerDashboardComponent, canActivate: [AuthGuard], data : {role: 'ROLE_FARMER'} },
+  { path: 'supplier', component: SupplierDashboardComponent, canActivate: [AuthGuard], data : {role: 'ROLE_SUPPLIER'} },
+  { path: 'view-complaint', component: ViewComplaintComponent, canActivate: [AuthGuard], data : {role: 'ROLE_ADMIN'} },
+  { path: 'update-complaint', component: UpdateComplaintComponent, canActivate: [AuthGuard], data : {role: 'ROLE_ADMIN'} },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' },
+  { path: 'view-complaint', component: ViewComplaintComponent },
+  { path: 'update-complaint', component: UpdateComplaintComponent, canActivate: [AuthGuard], data : {role: 'ROLE_ADMIN'} },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    AboutUsComponent,
+    AdminDashboardComponent,
+    SupplierDashboardComponent,
+    FarmerDashboardComponent,
+    ViewComplaintComponent,
+    UpdateComplaintComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+
+     // Add ReactiveFormsModule to the imports array
+  ],
+
+  // exports: [RouterModule],
+
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,62 @@
+
+
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit{
+
+  loginForm!: FormGroup;
+  errorMessage!: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onLogin() {
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    this.authService.login(email, password).subscribe(
+      (response) => {
+        // Assuming the response contains the token
+        const token = response.token;
+        this.authService.saveToken(token);
+        const decodedToken = this.authService.decodeToken();
+        const userRole = decodedToken.role;
+        console.log(userRole[0].authority);
+
+        // Redirect according to the role
+        if (userRole[0].authority === 'ROLE_ADMIN') {
+          this.router.navigate(['/admin']); // Replace '/admin' with your admin page route
+        } else if (userRole[0].authority === 'ROLE_FARMER') {
+          this.router.navigate(['/farmer']); // Replace '/farmer' with your farmer page route
+        } else if (userRole[0].authority === 'ROLE_SUPPLIER') {
+          this.router.navigate(['/supplier']); // Replace '/supplier' with your supplier page route
+        }
+      },
+      (error) => {
+        this.errorMessage = 'Invalid email or password.';
+      }
+    );
+  }
+
+ 
+}
